c901c924752341e045471e2a7f2221d7c86026e8
unknown <cbilson@.(none)>
2008-10-14 04:17:48 -0700

    Fixed AppContainer - I wish I knew how to chain together method invocations in f# more elegantly

--- FSharpMonoRail/AppContainer.fs
+++ FSharpMonoRail/AppContainer.fs
@@ -13,11 +13,10 @@ type AppContainer() as x =
     inherit WindsorContainer(new XmlInterpreter())
     
     do 
-        x.RegisterControllers()
-    
-    member private x.RegisterControllers : unit = 
-        let controllers = AllTypes.Of<Controller>()
-        let basedOn = controllers.FromAssembly(x.GetType().Assembly)
+        x.RegisterAll (AllTypes.Of<Controller>())
+        
+    member x.RegisterAll (types:AllTypesOf) =
+        let basedOn = types.FromAssembly(x.GetType().Assembly)
         let registrations = basedOn :> IRegistration
         registrations.Register(x.Kernel)
         
