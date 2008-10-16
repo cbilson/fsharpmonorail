4c4bf32475cf85a32701002c73057e877f8ca52f
Chris Bilson <cbilson@pobox.com>
2008-10-16 05:48:22 -0700

    made Global.asax.fs a code behind

--- FSharpMonoRail/Global.asax
+++ FSharpMonoRail/Global.asax
@@ -1,1 +1,11 @@
-<%@ Application Language="C#" Inherits="FSharpMonoRail.Application" %>
+<%@ Application CodeBehind="Global.asax.fs" Inherits="FSharpMonoRail.Application" Language="F#" %>
+
+<script RunAt="server" Language="f#">
+
+(*
+member x.Application_Start(sender:obj, args:EventArgs) =
+    System.Diagnostics.Debugger.Break()
+    printfn "here"
+*)
+  
+</script>
---
+++ FSharpMonoRail/Global.asax.fs
@@ -0,0 +1,22 @@
+﻿#light
+
+namespace FSharpMonoRail
+
+open System
+open System.Web
+open Castle.Windsor
+
+type Application() as x =
+    inherit System.Web.HttpApplication()
+    
+    [<DefaultValue(false)>]
+    static val mutable container : AppContainer option
+    
+    member x.Application_Start () =
+        if Application.container = None then Application.container <- Some(new AppContainer())
+    
+    interface IContainerAccessor with
+        member x.Container 
+            with get() = Option.get Application.container :> IWindsorContainer
+    
+        
