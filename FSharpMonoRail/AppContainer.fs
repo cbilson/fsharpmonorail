7b3b1393a8ff38f3af0350619f5da906f20fc6f9
Chris Bilson <cbilson@pobox.com>
2008-10-16 05:46:45 -0700

    added MonoRailFacility

--- FSharpMonoRail/AppContainer.fs
+++ FSharpMonoRail/AppContainer.fs
@@ -6,6 +6,7 @@ open System.Reflection
 open Castle.Core
 open Castle.MicroKernel.Registration
 open Castle.MonoRail.Framework
+open Castle.MonoRail.WindsorExtension
 open Castle.Windsor
 open Castle.Windsor.Configuration.Interpreters
 
@@ -13,7 +14,13 @@ type AppContainer() as x =
     inherit WindsorContainer(new XmlInterpreter())
     
     do 
-        x.RegisterAll (AllTypes.Of<Controller>())
+        x.RegisterFacilities()
+        x.RegisterControllers()
+        
+    member x.RegisterFacilities() =
+        x.AddFacility<MonoRailFacility>() |> ignore
+        
+    member x.RegisterControllers() = x.RegisterAll(AllTypes.Of<Controller>())
         
     member x.RegisterAll (types:AllTypesOf) =
         let basedOn = types.FromAssembly(x.GetType().Assembly)
