bdc1f2f3c575bd09412fe362b631ddf93a8be83a
Chris Bilson <cbilson@pobox.com>
2008-10-17 03:43:26 -0700

    added a little more to the domain model

---
+++ FSharpMonoRail/Model/Rate.fs
@@ -0,0 +1,12 @@
+﻿#light
+namespace FSharpMonoRail.Model
+
+type Rate() =
+    let mutable _value = 0M
+
+    abstract Value : decimal with get, set
+    
+    default x.Value
+        with get() = _value
+         and set(v:decimal) = _value <- v
+
