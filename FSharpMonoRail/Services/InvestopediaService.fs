ca7283d8b2f0b9cd6856bf4b86d84136094f160d
Chris Bilson <cbilson@pobox.com>
2008-10-21 03:54:17 -0700

    added InvestopediaService

---
+++ FSharpMonoRail/Services/InvestopediaService.fs
@@ -0,0 +1,23 @@
+﻿#light
+
+namespace FSharpMonoRail.Services
+
+open System
+open System.IO
+open SyStem.Net
+open FSharpMonoRail.Model
+
+type IInvestopediaService =
+    abstract LoginToInvestopedia: userName:string -> password:string -> unit
+    
+type InvestopediaService =
+
+    interface IInvestopediaSerivce with
+        member LoginToInvestopedia userName password =
+            let url = "http://simulator.investopedia.com/home.aspx"
+            let request = WebRequest.Create(url)
+            request.
+    
+    
+    
+    
